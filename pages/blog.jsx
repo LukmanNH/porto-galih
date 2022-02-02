d0af9333ac3307e48ce0580a1ae9878892cbf6bc
LukmanNH <luckmanhakim004@gmail.com>
2022-02-02 13:16:06 +0700

    writing journey desktop

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -2,35 +2,92 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className="bg-hero bg-no-repeat bg-cover bg-center h-[388px] ">
-      <div className="container-w-full mx-auto px-56 pt-6">
-        <div className="flex justify-between">
-          <p className="text-white">👈 Back to homepage</p>
-          <ul className="flex text-white space-x-[3.75rem]">
-            <li>Home</li>
-            <li>Project</li>
-            <li>Blog</li>
-            <li>About</li>
-          </ul>
-        </div>
-        <div className="flex mt-40">
-          <div className="w-7/12 mr-24">
-            <h1 className="text-white text-5xl">✏️ Writing Journey</h1>
-            <p className="mt-5 text-white font-light text-lg tracking-wider">
-              Something new I'm starting for 2021 — every week or two I write
-              about what I've been working on, problems I've solved.
-            </p>
+    <>
+      <div className="bg-hero bg-no-repeat bg-cover bg-center h-[388px]">
+        <div className="container-w-full mx-auto px-56 pt-6">
+          <div className="flex justify-between">
+            <p className="text-white">👈 Back to homepage</p>
+            <ul className="flex text-white space-x-[3.75rem]">
+              <li>Home</li>
+              <li>Project</li>
+              <li>Blog</li>
+              <li>About</li>
+            </ul>
           </div>
-          <img
-            src="blog-img.png"
-            className="-translate-y-8"
-            width={219}
-            height={242}
-            alt=""
-          />
+          <div className="flex">
+            <div className="w-7/12 mr-24 mt-40">
+              <h1 className="text-white text-5xl font-bold">
+                ✏️ Writing Journey
+              </h1>
+              <p className="mt-10 text-white font-light text-lg tracking-wider">
+                Something new I'm starting for 2021 — every week or two I write
+                about what I've been working on, problems I've solved.
+              </p>
+            </div>
+            <div>
+              <img
+                src="blog-img.png"
+                className="translate-y-36"
+                width={345}
+                height={226}
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="container-w-full mx-auto px-56 pt-6">
+        <select className="w-full py-3 lg:py-3 lg:w-1/5 border-2 mr-6 p-3 border-[#01549F] rounded-lg">
+          <option value={"Misal"}>All Categories</option>
+        </select>
+        <div class="relative inline-block text-left">
+          <div>
+            <button
+              type="button"
+              class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+              id="menu-button"
+              aria-expanded="true"
+              aria-haspopup="true"
+            >
+              Options
+              <svg
+                class="-mr-1 ml-2 h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div
+            class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button"
+            tabindex="-1"
+          >
+            <div class="py-1" role="none">
+              <a
+                href="#"
+                class="text-gray-700 block px-4 py-2 text-sm"
+                role="menuitem"
+                tabindex="-1"
+                id="menu-item-0"
+              >
+                Account settings
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
