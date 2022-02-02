556c09f5ee8f8cfe58fa48e069a767bd0be475cb
LukmanNH <luckmanhakim004@gmail.com>
2022-02-02 12:30:52 +0700

    home page desktop

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <>
       <div className="container-w-full bg-no-repeat bg-center bg-cover bg-hero h-[388px] flex items-stretch">
-        <div className="self-center w-10/12 container mx-auto px-28 pt-28">
+        <div className="self-center w-7/12 container mx-[14.375rem] pt-28">
           <p className="md:text-[2.5rem] sm:text-[2rem] tracking-wide lg:text-[2.875rem] font-sans text-white mx-auto font-bold">
             Hey, I'm Galih Putra. A struggling Digital Product Designer.
           </p>
         </div>
       </div>
-      <div className="container mx-auto pl-28 pr-16 pt-[58px] self-center w-10/12">
-        <p className="text-2xl w-12/12 font-sans font-light text-[#282828] leading-[3rem]">
+      <div className="container mx-[14.375rem] pt-[58px] self-center w-8/12">
+        <p className="text-2xl font-sans font-light text-[#282828] leading-[3rem]">
           Currently a UX Designer working within amazing team at Digital Product
           Agency{" "}
           <span className="border-b-[3px] font-semibold border-blue-600 duration-200 hover:text-[#01549F] cursor-pointer">
@@ -37,24 +37,16 @@ export default function Home() {
         </p>
         <div className="my-20 font-medium w-auto flex text-lg text-[#969696]">
           <div className="mr-1 hover:underline hover:text-[#01549F]">
-            <Link href={"/"}>
-              Linkedin,
-            </Link>
+            <Link href={"/"}>Linkedin,</Link>
           </div>
           <div className="mr-1 hover:underline hover:text-[#303030]">
-            <Link href={"/"}>
-              Medium,
-            </Link>
+            <Link href={"/"}>Medium,</Link>
           </div>
           <div className="mr-1 hover:underline hover:text-[#DB4EA3]">
-            <Link href={"/"}>
-              Dribbble,
-            </Link>
+            <Link href={"/"}>Dribbble,</Link>
           </div>
           <div className="hover:underline hover:text-[#EB6708]">
-            <Link href={"/"}>
-              Resume
-            </Link>
+            <Link href={"/"}>Resume</Link>
           </div>
         </div>
       </div>
