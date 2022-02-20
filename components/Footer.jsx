9271c397a3ba2725f433da69c7f18193cdb67d52
LukmanNH <luckmanhakim004@gmail.com>
2022-02-20 12:07:26 +0700

    Finishing About Me

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,23 +3,23 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="h-[23.625rem] sm:h-[24.625rem] md:h-[25.625rem] lg:h-[26.625rem] xl:h-[27.625rem] bg-[#F4F8FB] pt-[4.875rem] bottom-0 z-10">
-      <div className="w-11/12 md:w-10/12 mx-auto lg:w-9/12 xl:w-8/12">
+      <div className="w-11/12 md:w-10/12 mx-auto lg:w-9/12 xl:w-8/12 relative">
         <p className="font-bold text-[1.75rem] mb-[1.75rem]">
           See other selected projects
         </p>
-        <div className="flex items-center mb-6">
+        <div className="flex items-center cursor-pointer !m-0 border-transparent border-b-[0.188rem] hover:border-[#2B9EDE] w-[fit-content] !mb-[1.5rem]">
           <img src="../💰.svg" alt="" className="pr-1 h-4 lg:h-5" />
           Jenius App Re—design Concept for Competition
         </div>
-        <div className="flex items-center mb-6">
+        <div className="flex items-center cursor-pointer !m-0 border-transparent border-b-[0.188rem] hover:border-[#2B9EDE] w-[fit-content] !mb-[1.5rem]">
           <img src="../🎨.svg" alt="" className="pr-1 h-4 lg:h-5" />
           Telkomsel B2B Platfoam Design System
         </div>
-        <div className="flex items-center mb-6">
+        <div className="flex items-center cursor-pointer !m-0 border-transparent border-b-[0.188rem] hover:border-[#2B9EDE] w-[fit-content] !mb-[1.5rem]">
           <img src="../⚙.svg" alt="" className="pr-1 h-4 lg:h-5" />
           Telkomsel DigiHub API Marketplace Revamp
         </div>
-        <div className="flex items-center mb-9">
+        <div className="flex items-center cursor-pointer !m-0 border-transparent border-b-[0.188rem] hover:border-[#2B9EDE] w-[fit-content] !mb-[1.5rem]">
           <img src="../✂.svg" alt="" className="pr-1 h-4 lg:h-5" />
           UX Case Study — Barbershop Booking
         </div>
--- pages/AboutMe.jsx
+++ pages/AboutMe.jsx
@@ -11,45 +11,45 @@ const AboutMe = () => {
         alt=""
         className="top-0 absolute w-full z-0"
       />
-      <div className="pt-6 mx-auto w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 z-10 relative">
+      <div className="pt-6 ml-[14.375rem] w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 z-10 relative">
         <div className="text-[#282828] cursor-pointer text-base lg:text-lg font-medium flex items-center hover:border-b-[3px] border-[#2B9EDE] absolute">
           <img src="../👈.svg" alt="" className="mr-1 md:mr-2 h-4 lg:h-5" />
           <Link href={"/"}>Back to homepage</Link>
         </div>
-        <div className="pt-36 w-full z-0">
+        <div className="pt-[3.75rem] w-full z-0">
           <div className="grid">
             <div className="mb-10 text-4xl lg:text-[2.25rem] font-bold">
               Hay, I'm Galih Putra Windawan
             </div>
-            <p className="font-light text-sm md:text-base lg:text-lg text-[#282828] mb-8">
-              Currently I work as UX Designer at Digital Product Agency SALT
-              Indonesia and as a Software Engineering Student at BINUS
-              University — Day-to-day, I'm contributing on Digital Product
-              Interface, Design System, UI Kit and Product Quality Check. My
-              skills include digital product design, interactive design,
-              prototyping, mobile design, and web design.
+            <p className="font-light text-sm md:text-base lg:text-[1.25rem] text-[#282828] mb-8 lg:leading-10">
+              Currently I work as <b className="font-semibold">UX Designer</b>{" "}
+              at Digital Product Agency SALT Indonesia and as a{" "}
+              <b className="font-semibold">Software Engineering Student</b> at{" "}
+              <b className="font-semibold text-[#01549F]">BINUS University</b> —
+              Day-to-day, I'm contributing on Digital Product Interface, Design
+              System, UI Kit and Product Quality Check. My skills include
+              digital product design, interactive design, prototyping, mobile
+              design, and web design.
             </p>
             <div className="mx-auto h-[500px] bg-[#C4C4C4] w-[24.875rem] mb-8"></div>
-            <p className="font-light text-sm md:text-base lg:text-lg text-[#282828] mb-[1.875rem]">
+            <p className="font-light text-sm md:text-base lg:text-[1.25rem] text-[#282828] mb-[1.875rem] lg:leading-10">
               Beyond the office hours I’d love to playing online game such as
               Dota2, I'm quite confident in support role, and play various kinds
               of heroes like Eart Spirit, Crystal Maiden, and also I can play
               Luna but for support role haha.
             </p>
-            <p className="font-light text-sm md:text-base lg:text-lg text-[#282828] mb-[8.5rem]">
+            <p className="font-light text-sm md:text-base lg:text-[1.25rem] text-[#282828] lg:leading-10">
               I also spend my time on writing, I like to write about my personal
               development progress, and also what I'm working on — all my
-              writing journey can be read Here 👈, I also write all my career
-              journey so far Here 👈.
+              writing journey can be read{" "}
+              <b className="font-semibold text-[#01549F]">Here</b> 👈, I also
+              write all my career journey so far{" "}
+              <b className="font-semibold text-[#01549F]">Here</b> 👈.
             </p>
           </div>
         </div>
       </div>
-      <img
-        src="../Gelombang 2.svg"
-        alt=""
-        className="-bottom-2/3 absolute w-full z-[-1]"
-      />
+      <img src="../Gelombang 2.svg" alt="" className="w-full -mt-11" />
       <Footer />
     </div>
   );
