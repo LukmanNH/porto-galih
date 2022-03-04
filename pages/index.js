ede69149fc3f8742ed99b0efaf15c54b62ebabc8
LukmanNH <luckmanhakim004@gmail.com>
2022-03-04 15:26:35 +0700

    Another Final Touch

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,23 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta property="og:title" content="European Travel Destinations" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:description"
+          content="Offering tour packages for individuals or groups."
+        />
+        <meta
+          property="og:image"
+          content="http://euro-travel-example.com/thumbnail.jpg"
+        />
+        <meta
+          property="og:url"
+          content="http://euro-travel-example.com/index.htm"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <div className="container-w-full bg-no-repeat bg-center bg-cover bg-hero h-[15.25rem] sm:h-[18.25rem] md:h-[20.25rem] lg:h-[22.25rem] xl:h-[24.25rem] flex items-stretch">
         <div className="w-11/12 self-center mx-auto container pt-20 md:pt-24 lg:pt-28 md:w-10/12 lg:w-9/12 xl:w-8/12">
           <p className="text-[1.5rem] font-sans text-white mx-auto font-bold sm:text-[1.8rem] md:text-[2rem] lg:text-[2.5rem] tracking-wide xl:text-[2.875rem]">
