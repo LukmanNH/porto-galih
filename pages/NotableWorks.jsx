472abd8129069e636993231407f87335a3a4f250
LukmanNH <luckmanhakim004@gmail.com>
2022-03-31 00:00:47 +0700

    They Said This Is Final Touch *SIGH

--- pages/NotableWorks.jsx
+++ pages/NotableWorks.jsx
@@ -146,8 +146,7 @@ const NotableWorks = ({ getFirst2Project, getProject }) => {
                 Notable Works
               </div>
               <p className="mt-4 lg:mt-8 text-white font-light text-xs leading-[20px] sm:text-sm md:text-base lg:text-lg tracking-wider lg:leading-8">
-                Some project need password to gain access to the project,
-                contact{" "}
+                Some project need password to gain access, please contact{" "}
                 <span className="font-medium underline">
                   windawan26@gmail.com
                 </span>{" "}
